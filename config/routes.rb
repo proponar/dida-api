055a9a6a0f2c09efa13ac3a07f98a6629def8f73
Martin Povolny <mpovolny@redhat.com>
2020-07-06 10:35:16 +0200

    Add routest for entries and location.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    resource :entries
+    resource :locations do
+      collection do
+        get 'search'
+      end
+    end
+  end
 end
