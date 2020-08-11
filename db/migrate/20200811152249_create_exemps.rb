7927450219b003611a1fb8de0037594967739f47
Martin Povolny <mpovolny@redhat.com>
2020-08-11 18:33:49 +0200

    Add model Source.

--- db/migrate/20200811152249_create_exemps.rb
+++ db/migrate/20200811152249_create_exemps.rb
@@ -4,7 +4,6 @@ class CreateExemps < ActiveRecord::Migration[6.0]
       t.string :exemplifikace
       t.integer :author_id
       t.integer :entry_id
-      t.integer :author_id
       t.boolean :vetne
       t.integer :zdroj_id
       t.integer :lokalizace_obec #(vazebni: cislo obce, cislo casti obce, ), alternativne okres? a text
