ab9d30822c538690a8f88c457e8b176a5202d718
Martin Povolny <mpovolny@redhat.com>
2020-10-17 08:10:48 +0000

    Add missing exemps controller.

---
+++ app/controllers/api/exemps_controller.rb
@@ -0,0 +1,44 @@
+class Api::ExempsController < Api::BaseController
+  # The number of exemps for entry is in thousands, but each record is relatively short (hundreds bytes?)
+  # Do we need pagination here? What is the average length of the exemplification?
+  def index
+    entries = Exemp.where(:entry_id => params[:entry_id]).includes(:user).map(&:json_hash)
+    # entry = Entry.includes(:exemps => :user).where(:id => params[:entry_id])
+
+    #entries = entry.exemps.map(&:json_hash)
+    #   Entry.includes(:exemps => :user).where(:id => params[:entry_id])
+    #   Entry.includes(:exemps, :exemps => :user).where(:id => params[:entry_id]).map(&:json_hash)
+    render json: {message: 'Loaded all entries', data: entries}, status: 200
+  end
+
+
+  # POST   /api/entries/:entry_id/exemps(.:format)  api/exemps#create
+  #        /api/entries/9/exemps
+  def create
+    soft_params = params.permit!
+    e = Exemp.new({
+      :user     => current_user,
+      :entry_id => params[:entry_id],
+    }.update(
+      soft_params.slice(*%i(rok kvalifikator exemplifikace vyznam vetne aktivni rok urceni))
+    ))
+    e.save!
+    render json: { message: 'exemp created', data: e }, status: 201
+  end
+
+  def update
+    e = Exemp.find(params[:id])
+
+    soft_params = params.permit!
+    kod_obec = params[:lokalizace_obec_id]
+    e.update({
+      :user => current_user,
+    }.update(
+      soft_params.slice(*%i(rok kvalifikator exemplifikace vyznam vetne aktivni rok urceni))
+      )
+    )
+    render json: { message: 'exemp updated', data: e }, status: 200
+  rescue => e
+    render json: { message: "could not update exemp: #{e.message}" }, status: 400
+  end
+end
