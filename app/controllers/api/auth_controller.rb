a49f1bed3c008e7eaea7e2afa320e14289197fbd
Martin Povolny <mpovolny@redhat.com>
2021-07-20 19:36:02 +0200

    Initial work on Google auth.

--- app/controllers/api/auth_controller.rb
+++ app/controllers/api/auth_controller.rb
@@ -4,9 +4,15 @@ class Api::AuthController < ApplicationController
   def auth
     # read basic auth headers
     authenticate_or_request_with_http_basic('Administration') do |username, password|
-      user = User.find_by(:name => username, :password => password)
+      if params['requester_type'] == 'oauth'
+        # TODO: verify the token
+        user = User.find_or_create_user_by_jwt_token(username)
+      else
+        user = User.find_by(:name => username, :password => password)
+      end
+
       if user.blank?
-        render json: { message: 'Špatný login, nebo heslo.' }, status: 401
+        render json: { message: 'Špatný login, nebo heslo.' }, status: :unauthorized
         return
       end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,22 @@
 class User < ApplicationRecord
   has_many :entries
+
+  def self.find_or_create_user_by_jwt_token(token)
+    begin
+      decoded_token = JWT.decode(token, nil, false)
+      user_data = decoded_token.find do |el|
+        el['iss'] == "accounts.google.com"
+      end
+      email = user_data["email"]
+
+      u = User.find_by(:name => email)
+      if u.nil?
+        u = User.create(:name => user_data["email"], :token => SecureRandom.hex)
+        u.save!
+      end
+      u
+    rescue => e
+      return nil
+    end
+  end
 end
